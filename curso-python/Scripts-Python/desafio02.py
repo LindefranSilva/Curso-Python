62f51f7ef8e3b4c008bb6e2a340f2f7fa2508c4a
Lindefran Silva <lindefransilva91@gmail.com>
2026-02-05 10:23:02 -0300

    Corregindo-codigo

--- curso-python/Scripts-Python/desafio02.py
+++ curso-python/Scripts-Python/desafio02.py
@@ -1,4 +1,4 @@
-dia = int(input(17))
-mes = int(input('Agosto'))
-ano = int(input(1991))
+dia = int(input('Digite o dia do seu nascimento:'))
+mes = input('Digite o mes do nascimento:')
+ano = int(input('Digite o ano do nascimento:'))
 print('Você nasceu no dia {} do mes {} do ano de {}'.format(dia, mes, ano))
