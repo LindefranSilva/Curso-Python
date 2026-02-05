9c7d8368cfb0b4227fd158da38e5e14b79c9c2ae
Lindefran Silva <lindefransilva91@gmail.com>
2026-02-05 10:52:35 -0300

    Fiz, uma pequena alteração

--- curso-python/Scripts-Python/desafio02.py
+++ curso-python/Scripts-Python/desafio02.py
@@ -1,4 +1,4 @@
 dia = int(input('Digite o dia do seu nascimento:'))
 mes = input('Digite o mes do nascimento:')
 ano = int(input('Digite o ano do nascimento:'))
-print('Você nasceu no dia {} do mes {} do ano de {}'.format(dia, mes, ano))
+print('Você nasceu no dia {} do mes {} do ano de  {}. correto?'.format(dia, mes, ano))
